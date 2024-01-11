beaa8bdf805650cf01c8d8b17e5ee82b809e1bfe
saeed rajayi <Saeedrajayi@gmail.com>
2024-01-11 10:33:24 +0330

    Add project files.

---
+++ showPass/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace showPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chkhash = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 59);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(239, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 59);
+            this.textBox2.TabIndex = 1;
+            // 
+            // chkhash
+            // 
+            this.chkhash.AutoSize = true;
+            this.chkhash.Checked = true;
+            this.chkhash.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkhash.Location = new System.Drawing.Point(12, 17);
+            this.chkhash.Name = "chkhash";
+            this.chkhash.Size = new System.Drawing.Size(49, 17);
+            this.chkhash.TabIndex = 2;
+            this.chkhash.Text = "hash";
+            this.chkhash.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 123);
+            this.Controls.Add(this.chkhash);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox chkhash;
+    }
+}
+
---
+++ showPass/security.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Configuration;
+
+namespace showPass
+{
+    class security
+    {
+
+        /// <summary>
+        /// پنهان سازی یا رمز گذاری
+        /// </summary>
+        /// <param name="toEncrypt"></param>
+        /// <param name="useHashing"></param>
+        /// <returns></returns>
+        public static string Encrypt(string toEncrypt, bool useHashing)
+        {
+            try
+            {
+                byte[] keyArray;
+                byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+                System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
+                // Get the key from config file
+                string key = "##";
+                //System.Windows.Forms.MessageBox.Show(key);
+                if (useHashing)
+                {
+                    MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    hashmd5.Clear();
+                }
+                else
+                    keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.Key = keyArray;
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = tdes.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                tdes.Clear();
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch { return ""; }
+        }
+        /// <summary>
+        /// آشکار سازی یا رمزنگاری
+        /// </summary>
+        /// <param name="cipherString"></param>
+        /// <param name="useHashing"></param>
+        /// <returns></returns>
+        public static string Decrypt(string cipherString, bool useHashing)
+        {
+            try
+            {
+                if (cipherString != "")
+                {
+                    byte[] keyArray;
+
+                    byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+                    AppSettingsReader settingsReader = new AppSettingsReader();
+                    //Get your key from config file to open the lock!
+                    string key = "##";
+
+                    if (useHashing)
+                    {
+                        MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                        keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                        hashmd5.Clear();
+                    }
+                    else
+                        keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+                    TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                    tdes.Key = keyArray;
+                    tdes.Mode = CipherMode.ECB;
+                    tdes.Padding = PaddingMode.PKCS7;
+
+                    ICryptoTransform cTransform = tdes.CreateDecryptor();
+                    byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                    tdes.Clear();
+                    return UTF8Encoding.UTF8.GetString(resultArray);
+                }
+                else
+                    return "";
+            }
+            catch { return ""; }
+        }
+    }
+}
